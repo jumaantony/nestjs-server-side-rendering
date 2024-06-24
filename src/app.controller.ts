9def459dedfd909f545fdf5b25ce8856e3aaa040
Juma Antony <jumaantony840@gmail.com>
2024-06-24 16:23:02 +0300

    working on views

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,23 @@ export class AppController {
 
   @Get()
   @Render('index') // render handlebars template/views
-  index() {}
-  
+  index() {
+    return {
+      title: "Home"
+    }
+  }
+
+  @Get('/about')
+  @Render('about')
+  about(){
+    let viewData = []
+    viewData["description"] = "This app is a project demo for server side rendering"
+    viewData["author"] = "Developed by: Juma Anton"
+    const title = "About"
+    return {
+      title: title,
+      subtitle: "About us",
+      viewData: viewData
+    }
+  } 
 }
