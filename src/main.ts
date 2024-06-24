3629cee1c642841aee6ef51753b794069a340e03
Juma Antony <jumaantony840@gmail.com>
2024-06-24 11:23:45 +0300

    adding handlebars and utls

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.setBaseViewsDir(join(__dirname, '..', 'views'))
+  app.setViewEngine('hbs')
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'))
+  hbsUtils.registerWatchedPartials(join(__dirname, '..', 'views/layouts'))
+
   await app.listen(3000);
 }
 bootstrap();
